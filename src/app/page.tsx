62ca3769436428fa3cd7c14c9782319c49564c98
Maximilian Paju <maximilian.paju@gmail.com>
2025-06-10 19:11:53 +0200

    up package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { AsciiEffect } from "@/components/ascii";
 export default function Home() {
     return (
         <>
-            <section className="absolute z-10 flex max-w-screen-md flex-col p-8 xs:flex-wrap">
+            <section className="absolute z-10 flex max-w-(--breakpoint-md) flex-col p-8 xs:flex-wrap">
                 <div className="flex gap-2 items-center">
                     <AsciiEffect width={30} height={30} />
 
