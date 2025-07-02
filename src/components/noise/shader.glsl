b55ab9dd87c99529b3ee370155a80cbb9d9574a2
Maximilian Paju <maximilian.paju@gmail.com>
2025-07-02 22:59:34 +0200

    tweak

--- src/components/noise/shader.glsl
+++ src/components/noise/shader.glsl
@@ -3,26 +3,20 @@
 uniform float clock;
 uniform vec2 resolution;
 
-float color(float x) {
-    if (x < 20049.0 / 82979.0) {
+float colormap(float x) {
+    if (x < .25) {
         return 0.0;
-    } else if (x < 327013.0 / 810990.0) {
-        return (8546482679670.0 / 10875673217.0 * x - 2064961390770.0 / 10875673217.0) / 255.0;
+    } else if (x < .4125) {
+        return x * 1.25 - 20.0 / 255.0;
     } else if (x <= 1.0) {
-        return (103806720.0 / 483977.0 * x + 19607415.0 / 483977.0) / 255.0;
+        return x * 1.25 + 10.0 / 255.0;
     } else {
         return 1.0;
     }
 }
 
-vec4 colormap(float shade) {
-    float opacity = 0.1;
-    float color = color(shade) * opacity;
-    return vec4(color, color, color, 1.0);
-}
-
 float rand(vec2 n) { 
-    return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+    return fract(sin(dot(n, vec2(13.0, 4.015))) * 142500.0);
 }
 
 float noise(vec2 p){
@@ -34,28 +28,28 @@ float noise(vec2 p){
         mix(rand(ip), rand(ip + vec2(1.0, 0.0)), u.x), 
         mix(rand(ip + vec2(0.0, 1.0)), rand(ip + vec2(1.0, 1.0)), u.x), u.y);
 
-    return res*res;
+    return res * res;
 }
 
-const mat2 mtx = mat2(0.80,  0.60, -0.60,  0.80);
+const mat2 mat = mat2(0.80,  0.60, -0.60,  0.80);
 
 float brownian(vec2 p)
 {
     float f = 0.0;
 
-    f += 0.500000*noise(p + clock * 0.25 ); 
-    p = mtx*p*2.02;
-    f += 0.031250*noise(p); 
-    p = mtx*p*2.01;
-    f += 0.250000*noise(p); 
-    p = mtx*p*2.03;
-    f += 0.125000*noise(p); 
-    p = mtx*p*2.01;
-    f += 0.062500*noise(p); 
-    p = mtx*p*2.04;
-    f += 0.015625*noise(p + sin(clock * 0.5));
+    f += 0.5 * noise(p + clock * 0.25); 
+    p = mat * p * 2.05;
+    f += 0.315 * noise(p); 
+    p = mat * p * 2.0;
+    f += 0.250 * noise(p); 
+    p = mat * p * 2.05;
+    f += 0.125 * noise(p); 
+    p = mat * p * 2.0;
+    f += 0.0625 * noise(p); 
+    p = mat * p * 2.05;
+    f += 0.015 * noise(p);
 
-    return f/0.96875;
+    return f/0.975;
 }
 
 float pattern(in vec2 p)
@@ -65,7 +59,9 @@ float pattern(in vec2 p)
 
 void main()
 {
-    vec2 uv = gl_FragCoord.xy/resolution.x;
+    vec2 uv = gl_FragCoord.xy/resolution;
+    float opacity = 0.05;
     float shade = pattern(uv);
-    gl_FragColor = vec4(colormap(shade).rgb, 1.0);
+    float color = colormap(shade) * opacity;
+    gl_FragColor = vec4(color, color, color, 1.0);
 }
